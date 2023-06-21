85570292e2fe84ad501856b01696a82476b0c108
Sasha Lelyk <133365539+lelyyykk@users.noreply.github.com>
2023-06-21 14:24:54 +0300

    registration block

--- js/js.js
+++ js/js.js
@@ -20,3 +20,72 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+/*
+    ========================================
+    Validation
+    ========================================
+*/
+// Отримати посилання на форму
+var form = document.querySelector('form');
+
+// Додати обробник події 'submit' до форми
+form.addEventListener('submit', function(event) {
+    // Перевірка валідації перед відправкою форми
+    if (!validateForm()) {
+        event.preventDefault(); // Скасувати стандартну відправку форми, якщо валідація не пройдена
+    }
+});
+
+// Функція для валідації форми
+function validateForm() {
+    // Отримати значення полів форми
+    var nameInput = document.querySelector('input[name="name"]');
+    var surnameInput = document.querySelector('input[name="surname"]');
+    var phoneInput = document.querySelector('input[name="phone"]');
+    var interestInput = document.querySelector('input[name="interest"]');
+    var checkboxInput = document.querySelector('input[name="car"]');
+
+    // Перевірка валідації для кожного поля
+
+    // 1) Перевірка валідації для полів ім'я та прізвище
+    var name = nameInput.value;
+    var surname = surnameInput.value;
+    var nameRegex = /^[a-zA-Zа-яА-ЯёЁіІїЇєЄ'`]+$/; // Регулярний вираз для перевірки, що в полі можуть бути тільки букви
+
+    if (!nameRegex.test(name)) {
+        alert('Будь ласка, введіть дійсне ім\'я.');
+        return false;
+    }
+
+    if (!nameRegex.test(surname)) {
+        alert('Будь ласка, введіть дійсне прізвище.');
+        return false;
+    }
+
+    // 2) Перевірка валідації для поля з номером
+    var phone = phoneInput.value;
+    var phoneRegex = /^\d{12}$/; // Регулярний вираз для перевірки, що в полі можуть бути тільки 12 цифр
+
+    if (!phoneRegex.test(phone)) {
+        alert('Будь ласка, введіть дійсний номер телефону (12 цифр).');
+        return false;
+    }
+
+    // 3) Перевірка валідації для поля "Мене більше цікавить"
+    var interest = interestInput.value;
+    var validInterests = ['developer', 'QA'];
+
+    if (validInterests.indexOf(interest) === -1) {
+        alert('Будь ласка, введіть "developer" або "QA".');
+        return false;
+    }
+
+    // 4) Перевірка валідації для поля checkbox
+    if (!checkboxInput.checked) {
+        alert('Будь ласка, погодьтесь з Політикою Конфіденційності.');
+        return false;
+    }
+
+    return true; // Повертаємо true, якщо всі перевірки успішні
+}
